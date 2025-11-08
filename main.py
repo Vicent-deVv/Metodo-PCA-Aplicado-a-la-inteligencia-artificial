df0c148e35405897fffa888b3ae1050a4e7babab
Vicent-deVv <175661774+Vicent-deVv@users.noreply.github.com>
2025-11-08 18:06:45 -0500

    creacion del main

---
+++ main.py
@@ -0,0 +1,7 @@
+from utils.registro_asistencia import ejecutar_menu
+from utils.file_manager import asegurar_directorio_principal
+
+if __name__ == "__main__":
+    asegurar_directorio_principal() 
+    
+    ejecutar_menu()
